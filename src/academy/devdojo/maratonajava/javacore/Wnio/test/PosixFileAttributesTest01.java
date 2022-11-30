c39bd44ce617b862dbb8560b1a0617ef88e26fea
HoracioMurilo <101604089+HoracioMurilo@users.noreply.github.com>
2022-11-30 18:46:53 -0300

    Commit 30/11/2022

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/PosixFileAttributesTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PosixFileAttributesTest01.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.*;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
 
 public class PosixFileAttributesTest01 {
@@ -12,11 +15,10 @@ public class PosixFileAttributesTest01 {
         Path path = Paths.get("/home/william/dev/file.txt");
         PosixFileAttributes posixFileAttributes = Files.readAttributes(path, PosixFileAttributes.class);
         System.out.println(posixFileAttributes.permissions());
-        FileAttributeView fileAttributeView = Files.getFileAttributeView(path, PosixFileAttributeView.class);
-
-        Set<PosixFilePermission> posixFilePermissions = PosixFilePermissions.fromString("rw-rw-rw");
+        PosixFileAttributeView fileAttributeView = Files.getFileAttributeView(path, PosixFileAttributeView.class);
+        Set<PosixFilePermission> posixFilePermissions = PosixFilePermissions.fromString("rw-rw-rw-");
         fileAttributeView.setPermissions(posixFilePermissions);
-        System.out.println(fileAttributeView.readAttributes().permissions);
+        System.out.println(fileAttributeView.readAttributes().permissions());
 
 
     }
